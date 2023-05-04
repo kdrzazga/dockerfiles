46789eeaad42339c938cbaa839e5e558ab7867ca
KD <kdrzazga@luxoft.com>
2023-05-04 16:21:50 +0200

    Adding Programs for ada and lisp

---
+++ fat-alpine/lisp/virgil-poem.lisp
@@ -0,0 +1,14 @@
+(defun get-Virgil-poem ()
+  "Arma virumque cano, Troiae qui primus ab oris,
+  Italiam, fato profugus, Laviniaque venit
+  litora, multum ille et terris iactatus et alto
+  vi superum saevae memorem Iunonis ob iram;
+  multa quoque et bello passus, dum conderet urbem,
+  inferretque deos Latio, genus unde Latinum,
+  Albanique patres, atque altae moenia Romae.")
+
+; Example usage
+(write-line "The Virgil poem:")
+(print (get-Virgil-poem))
+;(write-line "Words of length 6:")
+;(write-line (extract-words-of-length (get-Virgil-poem) 6))
