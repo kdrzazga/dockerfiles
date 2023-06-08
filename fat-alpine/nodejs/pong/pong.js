9e17e47827706bef1aadc7e55f09735a0f3305fd
kdrzazga <kdrzazga@luxoft.com>
2023-06-08 11:14:29 +0200

    pong2

--- fat-alpine/nodejs/pong/pong.js
+++ fat-alpine/nodejs/pong/pong.js
@@ -1,7 +1,9 @@
+const clearScreen = require("clear-screen");
+
 class Ball {
   constructor() {
-    this.x = 1;
-    this.y = 3;
+    this.x = 8;
+    this.y = 5;
     this.directionH = 'right';
     this.directionV = 'down';
   }
@@ -16,28 +18,34 @@ class Board{
 
   moveBall(){
     this.checkDeflection();
-    if (this.ball.directionX === 'right')
+    if (this.ball.directionH === 'right')
       this.ball.x++;
     else
       this.ball.x--;
 
-    if (this.ball.directionY === 'down')
+    if (this.ball.directionV === 'down')
       this.ball.y++;
     else
       this.ball.y--;
-    
   }
 
   checkDeflection(){
-    if (this.ball.x <= 1)
+    if (this.ball.x <= 0){
       this.ball.directionH = 'right';
-    else if (this.ball.x >= board.sizeX - 2)
+      this.ball.x = 1;
+    }
+    else if (this.ball.x > board.sizeX - 4){
       this.ball.directionH = 'left';
-
-    if (this.ball.y <= 1)
-      this.ball.directionH = 'up';
-    else if (this.ball.y >= board.sizeY - 2)
-      this.ball.directionH = 'down';    
+      this.ball.x = board.sizeX - 3;
+    }
+    if (this.ball.y <= 0) {
+      this.ball.directionV = 'down';
+      this.ball.y = 1;
+    }
+    else if (this.ball.y > board.sizeY - 2){
+      this.ball.directionV = 'up';
+      this.ball.y = board.sizeY - 2;
+    }
   }
   
 }
@@ -47,18 +55,28 @@ class Drawer {
     console.log('#'.repeat(board.sizeX));
 
     for (var y = 0; y < board.sizeY; y++){
-      if (y === board.ball.y)
-        console.log('#' + ' '.repeat(board.ball.x) + 'o' + ' '.repeat(board.sizeX - board.ball.x - 3) + '#');
+      if (y === board.ball.y){
+		const repetition = Math.max(0, board.sizeX - board.ball.x - 3);
+        console.log('#' + ' '.repeat(board.ball.x) + 'o' + ' '.repeat(repetition) + '#');
+	  }
       else console.log('#' + ' '.repeat(board.sizeX - 2) + '#');  
     }
 
     console.log('#'.repeat(board.sizeX));
+    
+    console.log(`[${board.ball.x},${board.ball.y}]`);
   }
 }
 
-const board = new Board(new Ball);
+const board = new Board(new Ball());
 const drawer = new Drawer();
 
-drawer.draw(board);
-board.moveBall();
-drawer.draw(board);
+function main() {
+  setInterval(() => {
+    clearScreen();
+    drawer.draw(board);
+    board.moveBall();  
+  }, 300);
+}
+
+main();
