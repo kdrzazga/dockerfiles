9150da86444dbf6bee1c161faa15a278415002b1
kdrzazga <kdrzazga@luxoft.com>
2023-06-23 19:47:53 +0200

    new-game

--- jssrv/webapp2/be/controller.js
+++ jssrv/webapp2/be/controller.js
@@ -3,11 +3,31 @@ const { info } = require('./logic.js');
 
 const server = http.createServer((req, res) => {
 	
-	if (req.method === 'GET' && req.url === '/') {
-			res.statusCode = 200;
-			res.setHeader('Content-Type', 'text/plain');
+	if (req.method === 'GET'){
+		res.setHeader('Content-Type', 'text/plain');
+		
+		if(req.url === '/') {
+			res.statusCode = 200;			
 			res.end(info());
-	} 
+		}
+		else {
+			res.statusCode = 404;
+			res.end('wrong GET request');
+		}
+	}
+	
+	else if (req.method === 'POST'){
+		res.setHeader('Content-Type', 'text/plain');
+		
+		if(req.url === '/start' || req.url === '/reset') {
+			res.statusCode = 200;			
+			res.end('new game');
+		}
+		else {
+			res.statusCode = 404;
+			res.end('wrong POST request');
+		}
+	}
 	
 });
 
--- jssrv/webapp2/be/logic.js
+++ jssrv/webapp2/be/logic.js
@@ -1,5 +1,5 @@
 function info(){
-	return "webapp2";
+	return "Board Clicker";
 }
 
 module.exports={
