ae4dbc3fa8daebae324ae629c1d79e5cdb6810cd
kdrzazga <kdrzazga@luxoft.com>
2023-07-15 11:20:12 +0200

    pyvilization-3

--- fat-alpine/python/microservices/service2.py
+++ fat-alpine/python/microservices/service2.py
@@ -1,50 +1,9 @@
-from flask import Flask, jsonify
-from service2files.game import move, help, found_city
-from service2files.drawer import draw
-from service2files.gamemap import generate_map, get_map
+import subprocess
 
-app = Flask(__name__)
+directory = 'service2files'
 
+script_name = 'civilization.py'
 
-@app.route('/', methods=['GET'])
-def get_info_endpoint():
-    return 'pyvilization - a Civilization-like game in python'
+command = f'python {directory}/{script_name}'
 
-
-@app.route('/start-game', methods=['POST'])
-def start_game_endpoint():
-    map = generate_map()
-    
-    status = 200 if map is not None else 500
-    message = 'Done' if map is not None else 'Map generation failed'
-    
-    response = {'message': message}
-    return jsonify(response), status
-
-
-@app.route('/move', methods=['POST'])
-def move_endpoint():
-    #move()
-    pass
-
-
-@app.route('/found', methods=['POST'])
-def start_game():
-    #found_city()
-    pass
-
-
-@app.route('/help', methods=['GET'])
-def help_endpoint():
-    response = {'message' : help()}
-    return jsonify(response), 200
-
-
-@app.route('/map', methods=['GET'])
-def map_endpoint():
-    map = JSON.stringify(get_map())
-
-
-if __name__ == '__main__':
-    print("starting service on 9982")
-    app.run(port=9982)
+subprocess.run(command, shell=True)
