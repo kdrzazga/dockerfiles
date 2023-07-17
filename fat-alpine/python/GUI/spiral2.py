d557946c4ac3ca3f90c4bb5fdf50079a82915dcb
kdrzazga <kdrzazga@luxoft.com>
2023-07-17 10:04:41 +0200

    spiral

--- fat-alpine/python/GUI/spiral2.py
+++ fat-alpine/python/GUI/spiral2.py
@@ -10,9 +10,10 @@ pygame.display.set_caption("Pygame Spiral")
 
 clock = pygame.time.Clock()
 
+BLACK = (0, 0, 0)
 start_radius = 10
 angle_increment = 0.1
-distance_increment = 0.1
+distance_increment = 0.03
 color = (128, 128, 0)
 center_x = window_width // 2
 center_y = window_height // 2
@@ -22,13 +23,14 @@ angle = 0
 distance = start_radius
 prev_x = center_x
 prev_y = center_y
+coeff = 1
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    x = center_x + distance * math.cos(angle)
-    y = center_y + distance * math.sin(angle)
+    x = center_x + distance * angle/2*coeff*math.cos(angle)
+    y = center_y + distance * angle/coeff* math.sin(angle)
 
     pygame.draw.line(window, color, (int(prev_x), int(prev_y)), (int(x), int(y)))
 
@@ -40,6 +42,13 @@ while running:
     
     pygame.display.update()
 
-    clock.tick(300)
+    clock.tick(100)
+    
+    if distance > 60:
+        distance = start_radius
+        angle = 0
+        window.fill(BLACK)
+        coeff -= 0.5
+
 
 pygame.quit()
