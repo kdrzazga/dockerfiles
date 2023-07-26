cdfa087646bdbe330ae6762fa6a7d314110402b8
kdrzazga <kdrzazga@luxoft.com>
2023-07-26 22:12:35 +0200

    sprite3

--- fat-alpine/c64kickass/animsprite.asm
+++ fat-alpine/c64kickass/animsprite.asm
@@ -1,35 +1,120 @@
 .const enable_sprites = $d015
+
 .const sprite0_pointer = $07f8
 .const sprite0_x = $d000
 .const sprite0_y = $d001
-.const sprite0_color = $d027
 
-//10 SYS2061
+.const sprite1_pointer = $07f9
+.const sprite1_x = $d002
+.const sprite1_y = $d003
+
+.const sprite2_pointer = $07fa
+.const sprite2_x = $d004
+.const sprite2_y = $d005
+
+.const sprite3_pointer = $07fb
+.const sprite3_x = $d006
+.const sprite3_y = $d007
+
+.const sprite4_pointer = $07fc
+.const sprite4_x = $d008
+.const sprite4_y = $d009
+
+.const sprite0_color = $d027
+.const sprite1_color = $d028
+.const sprite2_color = $d029
+.const sprite3_color = $d02a
+.const sprite4_color = $d02b
+
+.const sprite_height_ptr = $d017
+.const sprite_width_ptr = $d01d
+
+.const raster_ptr = $d012
+.const counter = $fa // a zeropage address to be used as a counter
+.const sys = $9e00
+.const init_basic = $080b
+
 *=$0801
-    .byte $0B, $08, 10, $00, $9E
+    .word init_basic
+	.byte 10
+	.word sys
 	.text "2061"
 	.byte $00, $00, $00
 
 *=2061
 
-lda #$80
-sta sprite0_pointer
-
 lda #3
 sta sprite0_color
+lda #7
+sta sprite1_color
+lda #13
+sta sprite2_color
+lda #14
+sta sprite3_color
+sta sprite4_color
 
-lda #%00000001
+lda #%00011111
 sta enable_sprites
 
 lda #99
 sta sprite0_x
 sta sprite0_y
-rts
+sta sprite1_y
+sta sprite2_x
 
-wait:  lda #$fb    // wait for vertical retrace
-loop2:  cmp $d012   // until it reaches 251th raster line ($fb)
+lda #130
+sta sprite1_x
+sta sprite2_y
+sta sprite3_x
+sta sprite3_y
+
+lda #150
+sta sprite4_x
+sta sprite4_y
+
+lda #%00001100
+sta sprite_height_ptr
+
+lda #%00011000
+sta sprite_width_ptr
+
+ldx #$80
+
+delay:  lda #$fb    // wait for vertical retrace
+loop2:  cmp raster_ptr   // until it reaches 251th raster line ($fb)
         bne loop2   // which is out of the inner screen area
-		rts
+
+        inc counter // increase frame counter
+        lda counter // check if counter
+        cmp #6      // reached 6
+        bne out     // if not, pass the switching routine
+
+        lda #$00    // reset
+        sta counter // counter
+
+loop:	
+		stx sprite0_pointer
+		stx sprite1_pointer
+		stx sprite2_pointer
+		stx sprite3_pointer
+		stx sprite4_pointer
+
+		inx
+	
+		cpx #$83
+		beq reset_sprite_loop
+		
+		jmp delay
+
+reset_sprite_loop:
+		ldx #$80
+		jmp delay		
+out:
+        lda raster_ptr // make sure we reached
+loop3:  cmp raster_ptr // the next raster line so next time we
+        beq loop3 // should catch the same line next frame
+
+        jmp delay
 
 *=$2000
 // sprite 0 / singlecolor / color: $01
