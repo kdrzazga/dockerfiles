dc76d7448268caf0f97c04c7f4a6df65f86809d5
kdrzazga <kdrzazga@luxoft.com>
2023-08-06 15:49:36 +0200

    game-scroll4-commando

--- fat-alpine/python/GUI/bitmap_scroll/scroll.py
+++ fat-alpine/python/GUI/bitmap_scroll/scroll.py
@@ -7,28 +7,24 @@ canvas_height = 200
 screen = pygame.display.set_mode((canvas_width, canvas_height))
 pygame.display.set_caption("Seemingly Infinite Scrolling with Animated Soldier")
 
-# Load the scrolling background bitmap
-background_bitmap = pygame.image.load("background_image.png")  # Replace "background_image.png" with your image file path
+background_bitmap = pygame.image.load("background_image.png")
 
-# Create a surface to hold two copies of the scrolling background side by side
 double_bitmap_width = background_bitmap.get_width() * 2
 double_bitmap = pygame.Surface((double_bitmap_width, background_bitmap.get_height()))
-double_bitmap.blit(background_bitmap, (0, 0))  # Copy the original background
-double_bitmap.blit(background_bitmap, (background_bitmap.get_width(), 0))  # Copy the original background again next to it
+double_bitmap.blit(background_bitmap, (0, 0))
+double_bitmap.blit(background_bitmap, (background_bitmap.get_width(), 0))
 
 background_x = 0
 
-# Load the animated soldier frames
-soldier_frames = [pygame.image.load(f"soldier_frame{i}.png").convert_alpha() for i in range(1, 5)]  # Assumes files named soldier_frame1.png, soldier_frame2.png, ...
+soldier_frames = [pygame.image.load(f"soldier_frame{i}.png").convert_alpha() for i in range(1, 5)]
 
 soldier_x = 20
 soldier_y = 98
 current_frame = 0
-animation_speed = 8  # Adjust this value to control the animation speed
 
 running = True
 clock = pygame.time.Clock()
-scroll_speed = 5  # Adjust this value to control the scrolling speed
+scroll_speed = 5
 soldier_anim_counter = 0
 
 while running:
@@ -38,18 +34,13 @@ while running:
 
     screen.fill((0, 0, 0))  # Black background
 
-    # Draw the scrolling background
-    screen.blit(double_bitmap, (background_x, 30))  # You can adjust the y-coordinate (30 in this case)
-
-    # Draw the animated soldier
+    screen.blit(double_bitmap, (background_x, 30))
+    
     screen.blit(soldier_frames[current_frame], (soldier_x, soldier_y))
 
-    # Update the scrolling background position
     background_x -= scroll_speed
     if background_x <= -background_bitmap.get_width():
         background_x = 0
-
-    # Update the animated soldier frame
     
     if soldier_anim_counter > 7:
         current_frame = (current_frame + 1) % len(soldier_frames)
