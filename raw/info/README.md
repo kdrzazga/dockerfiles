86c57065d1cd828b02e2deb90250cec9a14f7470
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-08-27 12:05:15 +0200

    raw-disk-image (#41)

---
+++ raw/info/README.md
@@ -0,0 +1,23 @@
+### Simple bootloader
+
+1. Build Docker image from Dockerfile
+`docker build -t boot2 .`
+
+2. Run the image
+`docker run -it boot2`
+
+3. Using Docker Desktop download file `/usr/src/bootloader/bootloader.img`
+
+![Docker Desktop](bootloader.img.png)
+
+4. Run Oracle VM VirtualBox add a new image with the following parameters:
+
+System -> Motherboard -> Boot Order  = (only) Floppy
+
+![Boot Order](VirtualBox.bootorder.png)
+
+Storage -> Storage Devices Controller Floppy with 'bootloader.img'
+
+![Storage](VirtualBox.storage.png
+
+5. Start VirtualBox image
---
+++ raw/sector2.asm
@@ -0,0 +1,30 @@
+[org 0x7e00]
+
+BIOS_TELETYPE_OUT	equ 0x0E
+RED		equ 4
+
+    xor ax, ax              ; Clear AX
+    mov ds, ax              ; Set data segment to 0
+    mov es, ax              ; Set extra segment to 0
+    mov si, msg             ; Set SI to point to the message
+
+print:
+    lodsb                   ; Load character from SI into AL
+    or al, al               ; Check for null terminator
+    jz done
+	
+	mov ah, BIOS_TELETYPE_OUT
+    mov bh, 0              ; Page number (usually 0)
+    mov bl, RED
+    int 0x10
+
+    jmp print
+
+done:
+    jmp $                  ; Infinite loop to exit
+
+msg:
+    db 10, 10, 13, "This is data read from sectors 2 - 10", 10, 13, 0 ; Null-terminated message string
+
+
+times 10*512-($-$$) db 0     ; Fill the rest of 10 sectors with NULLs
