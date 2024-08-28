18f8d059ab87286481dcd82b839b11c28675b3e4
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-08-28 07:22:04 +0200

    raw-images (#42)

---
+++ raw/bios-pong/mbr.asm
@@ -0,0 +1,93 @@
+[org 0x7C00]
+
+BIOS_TELETYPE	equ 0x0E
+ENTER_KEY	equ 0x0D
+ESC_KEY	equ 0x1B
+LIGHT_GREEN   		equ 0x0A
+PAGE_NUM      		equ 0      ; Page number, usually 0
+BIOS_READ_SECTOR	equ 0x02
+BIOS_READ_KEYBOARD	equ 0x00
+BIOS_RESET	equ 0x0000
+
+; Set graphical mode 320x200, 16 colors
+mov ax, 0x0004 
+int 0x10
+
+xor ax, ax              ; Clear AX
+mov ds, ax              ; Set data segment to 0
+mov es, ax              ; Set extra segment to 0
+mov si, msg             ; Set SI to point to the message
+
+print:
+    lodsb               ; Load character from SI into AL
+    or al, al          ; Check for null terminator
+    jz wait_for_key
+
+    mov ah, BIOS_TELETYPE
+    mov bh, PAGE_NUM
+    mov bl, LIGHT_GREEN
+    int 0x10
+
+    jmp print
+
+wait_for_key:
+    ; Wait for a key press
+    mov ah, BIOS_READ_KEYBOARD
+    int 0x16
+
+    cmp al, ENTER_KEY
+    je read_second_sector
+
+    cmp al, ESC_KEY 
+    je reset 
+
+    jmp wait_for_key
+
+read_second_sector:
+    ; Read the second sector (cylinder 0, head 0, sector 2)
+    mov ah, BIOS_READ_SECTOR
+    mov al, 10          ; Number of sectors to read
+    mov ch, 0          ; Cylinder 0
+    mov cl, 2          ; Sector 2
+    mov dh, 0          ; Head 0
+    mov bx, 0x7E00     ; Buffer to read data into (0x7E00)
+    int 0x13           ; Call BIOS interrupt to read the sector
+
+    ; Check if the read operation was successful
+    jc error           ; If CF is set, jump to error handling
+
+    ; Jump to the loaded code at 0x7E00
+    jmp 0x7E00         ; Execute code from the second sector
+
+error:
+    ; Handle error (e.g., infinite loop)
+    mov si, error_msg
+    call print_error
+    jmp $              ; Halt the system
+
+reset:
+    mov ax, BIOS_RESET
+    int 0x19
+
+print_error:
+    ; Print the error message
+    mov si, error_msg
+error_print:
+    lodsb              ; Load character from error message into AL
+    or al, al         ; Check for null terminator
+    jz done_error      ; If null terminator, jump to done
+    mov ah, BIOS_TELETYPE;
+    int 0x10          ; Print character
+    jmp error_print    ; Loop back to print next character
+
+done_error:
+    jmp $              ; Infinite loop to exit
+
+msg:
+    db "Welcome to BIOS_PONG game.", 10, 10, 13, "Press ENTER to continue.", 10, 13, 0; Null-terminated message string
+
+error_msg:
+    db "Error reading sector.", 10, 0 ; Null-terminated error message string
+
+times 510-($-$$) db 0     ; Fill the rest of the sector with zeros
+dw 0xAA55                 ; Boot signature END OF MASTER BOOT RECORD
---
+++ raw/bios-pong/pong.asm
@@ -0,0 +1,69 @@
+[org 0x7e00]
+
+WHITE         equ 15                ; Color for the ball
+LIGHT_GRAY    equ 8                 ; Color for the bat
+BIOS_TELETYPE	equ 0x0E
+LIGHT_GREEN	equ 0x0A
+LIGHT_BLUE   		equ 9
+PAGE_NUM	equ 0
+
+mov ax, 0x000e       
+int 0x10
+
+loop:
+	call clrscr
+	call draw_bat
+	call draw_ball
+    mov ah, 0		;wait key
+    int 0x16
+	
+    reset:
+    mov ax, 0x0000
+    int 0x19
+
+clrscr:
+    mov ax, 0xa000        ; Point to video memory
+    mov es, ax            ; Set ES to point to the video segment
+    xor di, di            ; Clear DI (offset within video memory)
+    mov cx, 64000         ; Number of bytes to fill (320 * 200)
+
+private:
+    mov al, 255           ; Set AL to 0 (to fill black)
+	black_fill:    
+		mov [es:di], al ; Fill the byte with black
+		inc di               ; Move to the next pixel
+		loop black_fill       ; Decrement CX and continue until zero
+	ret
+
+draw_bat:
+	push bx
+    mov ax, 0xA000          ; Segment address for video memory
+    mov es, ax              ; Set ES to point to video memory
+    xor di, di              ; Clear DI (offset)
+
+    mov ax, [bat_y]         ; Use word for position
+    mov cx, 320
+    mul cx                  ; Y * 320 (width)
+    add di, ax              ; Add y offset
+    add di, [bat_x]         ; Add x position
+    ; No shl needed if in 256-color mode (we'll use bytes)
+
+    mov cx, [bat_width]     ; Assuming you have defined width
+    mov al, LIGHT_GREEN       ; Load color into AL
+    draw_bat_loop:
+        mov [es:di], bl       ; Set the pixel color
+        inc di                ; Next pixel
+        loop draw_bat_loop    ; Loop through width
+    pop bx
+	ret
+	
+draw_ball:
+	ret
+
+bat_width: dw 20  
+ball_x: dw 120
+ball_y: dw 120
+bat_x: dw 10
+bat_y: dw 100
+
+times 10*512-($-$$) db 0     ; Fill the rest of 10 sectors with NULLs
