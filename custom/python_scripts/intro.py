da6fafe8ad236bf17f0cfdf7260e6501826507c0
kdrzazga <kdrzazga@luxoft.com>
2024-08-30 10:59:06 +0200

    custom-python-image

---
+++ custom/python_scripts/intro.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import art, sys, os
+
+from colorama import Fore
+
+welcome = art.text2art("H E L L O !")
+print(f"{Fore.CYAN}{welcome}{Fore.RESET}")
+
+for msg in ("This image is a kind of adding custom commands to Linux.", "Those custom commands are in fact python programs, set to be ran by python interepreter automatically by typing their name.", "This was achieved by adding run header (#!/usr/bin/env python) to the scripts and adding the scripts directory to the PATH.", ""):
+    print(msg)
+
+for msg in ("Available commands:", "", "program1", "program2", "intro - displays this intro", ""):
+    print(msg)
+    
+if len(sys.argv) > 1 and sys.argv[1] == "up":
+    print("\nCURRENT USER: ")
+    os.system('whoami')
+    os.system('bash')
---
+++ custom/python_scripts/program2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import os
+
+os.system('clear')
+print("Program 2")
