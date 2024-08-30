83192fa103a488ce28db6ef925a77655a9bcc2bd
kdrzazga <kdrzazga@luxoft.com>
2024-08-30 17:52:21 +0200

    comic

--- custom/python_scripts/intro.py
+++ custom/python_scripts/intro.py
@@ -10,7 +10,7 @@ print(f"{Fore.CYAN}{welcome}{Fore.RESET}")
 for msg in ("This image is a kind of adding custom commands to Linux.", "Those custom commands are in fact python programs, set to be ran by python interepreter automatically by typing their name.", "This was achieved by adding run header (#!/usr/bin/env python) to the scripts and adding the scripts directory to the PATH.", ""):
     print(msg)
 
-for msg in ("Available commands:", "", "program1", "program2", "intro - displays this intro", ""):
+for msg in ("Available commands:", "", "program1", "program2", "comic", "intro - displays this intro", ""):
     print(msg)
     
 if len(sys.argv) > 1 and sys.argv[1] == "up":
