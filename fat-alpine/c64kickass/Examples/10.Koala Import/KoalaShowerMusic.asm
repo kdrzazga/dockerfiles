5506c08be9b9e5375c752d61151b2d0458aa5600
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-11-06 15:16:40 +0100

    More background music

---
+++ fat-alpine/c64kickass/Examples/10.Koala Import/KoalaShowerMusic.asm
@@ -0,0 +1,93 @@
+//.var music = LoadSid("Everlasting_Love.sid")
+.var music = LoadSid("Bach.sid")
+//.var music = LoadSid("Warrior_of_Ras_II-Kaiv.sid")
+:BasicUpstart2(start)
+
+			//.var picture = LoadBinary("nicecouple.kla", BF_KOALA)
+			.var picture = LoadBinary("cwaniaczki.kla", BF_KOALA)
+			//.var picture = LoadBinary("brownnoble.kla", BF_KOALA)
+
+start:  	lda #$38
+			sta $d018
+			lda #$d8
+			sta $d016
+			lda #%00111011
+			sta $d011
+			lda #BLACK
+			sta $d020
+			lda #picture.getBackgroundColor()
+			sta $d021
+			ldx #0
+
+loop1:		.for (var i=0; i<4; i++) {
+				lda colorRam+i*$100,x
+				sta $d800+i*$100,x
+			}
+			inx
+			bne loop1
+
+play_music:
+		
+            ldx #0
+            ldy #0
+            lda #music.startSong-1
+            jsr music.init
+            sei
+            lda #<irq1
+            sta $0314
+            lda #>irq1
+            sta $0315
+            asl $d019   //Interrupt status register
+            lda #%01111011
+            sta $dc0d   //Interrupt control and status register.
+            lda #%10000001
+            sta $d01a   //Interrupt control register
+            lda #%00111011
+            sta $d011   //Screen control register #1 - watch for conflict with value from lines 16 (16&17)
+            lda #%10000000
+            sta $d012   //Raster line to generate interrupt at (bits #0-#7).
+            cli
+
+this:
+    jmp this
+
+irq1:
+    asl $d019   //Interrupt status register
+    jsr music.play
+    pla
+    tay
+    pla
+    tax
+    pla
+
+rti
+
+*=$0c00	"ScreenRam"; 			.fill picture.getScreenRamSize(), picture.getScreenRam(i)
+*=$1c00	"ColorRam:"; colorRam: 	.fill picture.getColorRamSize(), picture.getColorRam(i)
+*=$2000	"Bitmap";				.fill picture.getBitmapSize(), picture.getBitmap(i)
+
+.pc=music.location "Music"
+.fill music.size, music.getData(i)
+
+.print ""
+.print "SID Data"
+.print "--------"
+.print "location=$"+toHexString(music.location)
+.print "init=$"+toHexString(music.init)
+.print "play=$"+toHexString(music.play)
+.print "songs="+music.songs
+.print "startSong="+music.startSong
+.print "size=$"+toHexString(music.size)
+.print "name="+music.name
+.print "author="+music.author
+.print "copyright="+music.copyright
+.print ""
+.print "Additional tech data"
+.print "--------------------"
+.print "header="+music.header
+.print "header version="+music.version
+.print "flags="+toBinaryString(music.flags)
+.print "speed="+toBinaryString(music.speed)
+.print "startpage="+music.startpage
+.print "pagelength="+music.pagelength
+
